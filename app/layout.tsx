610b8c7558325200c2845bf37fd0b57b1f3baa18
Dungha125 <dungha122405@gmail.com>
2025-01-27 10:59:19 +0700

    dungha

--- app/layout.tsx
+++ app/layout.tsx
@@ -25,7 +25,7 @@ export const metadata: Metadata = {
     siteName: "Duyên Hợp x Bảo Long",
     images: [
       {
-        url: "https://thiepmoi-hoplong.vercel.app/thum.jpg", // Ảnh preview khi chia sẻ link
+        url: "https://thiepmoi-hoplong.vercel.app/SVG/preview.jpg", // Ảnh preview khi chia sẻ link
         width: 1200,
         height: 630,
         alt: "Thiệp mời sự kiện Duyên Hợp x Bảo Long",
@@ -37,7 +37,7 @@ export const metadata: Metadata = {
     card: "summary_large_image",
     title: "THIỆP MỜI DUYÊN HỢP x BẢO LONG",
     description: "Hãy tham gia cùng chúng tôi trong sự kiện đặc biệt này!",
-    images: ["https://thiepmoi-hoplong.vercel.app/thum.jpg"], // Ảnh preview trên Twitter
+    images: ["https://thiepmoi-hoplong.vercel.app/SVG/preview.jpg"], // Ảnh preview trên Twitter
   },
 };
 
@@ -51,7 +51,7 @@ export default function RootLayout({
         <meta property="og:type" content="website" />
         <meta property="og:title" content="THIỆP MỜI DUYÊN HỢP x BẢO LONG" />
         <meta property="og:description" content="Hãy tham gia cùng chúng tôi trong sự kiện đặc biệt này!" />
-        <meta property="og:image" content="https://thiepmoi-hoplong.vercel.app/thum.jpg" />
+        <meta property="og:image" content="https://thiepmoi-hoplong.vercel.app/SVG/preview.jpg" />
         <meta property="og:url" content="https://thiepmoi-hoplong.vercel.app/" />
         <meta property="og:image:width" content="1200"/>
         <meta property="og:image:height" content="630"/>
@@ -59,7 +59,7 @@ export default function RootLayout({
         <meta name="twitter:card" content="summary_large_image" />
         <meta name="twitter:title" content="THIỆP MỜI DUYÊN HỢP x BẢO LONG" />
         <meta name="twitter:description" content="Hãy tham gia cùng chúng tôi trong sự kiện đặc biệt này!" />
-        <meta name="twitter:image" content="https://thiepmoi-hoplong.vercel.app/thum.jpg" />
+        <meta name="twitter:image" content="https://thiepmoi-hoplong.vercel.app//preview.jpg" />
       </head>
       <body className={`${geistSans.variable} ${geistMono.variable} antialiased`}>
         {children}
