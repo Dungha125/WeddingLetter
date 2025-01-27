0c635689a8b38bda43877fb1e677e258b3867624
Dungha125 <dungha122405@gmail.com>
2025-01-27 11:58:49 +0700

    dungha

--- app/layout.tsx
+++ app/layout.tsx
@@ -25,7 +25,7 @@ export const metadata: Metadata = {
     siteName: "Duyên Hợp x Chí Long",
     images: [
       {
-        url: "https://thiepmoi-hoplong.vercel.app/SVG/preview.jpg",
+        url: "https://www.facebook.com/photo/?fbid=1134088408374700&set=pcb.1134090915041116",
         width: 1200,
         height: 630,
         alt: "Thiệp mời sự kiện Duyên Hợp x Chí Long",
@@ -37,7 +37,7 @@ export const metadata: Metadata = {
     card: "summary_large_image",
     title: "THIỆP MỜI DUYÊN HỢP x CHÍ LONG",
     description: "Hãy tham gia cùng chúng tôi trong sự kiện đặc biệt này!",
-    images: ["https://thiepmoi-hoplong.vercel.app/SVG/preview.jpg"],
+    images: ["https://www.facebook.com/photo/?fbid=1134088408374700&set=pcb.1134090915041116"],
   },
 };
 
@@ -51,8 +51,8 @@ export default function RootLayout({
         <meta property="og:type" content="website" />
         <meta property="og:title" content="THIỆP MỜI DUYÊN HỢP x CHÍ LONG" />
         <meta property="og:description" content="Hãy tham gia cùng chúng tôi trong sự kiện đặc biệt này!" />
-        <meta property="og:image" content="https://thiepmoi-hoplong.vercel.app/SVG/preview.jpg" />
-        <meta property="og:image:secure_url" content="https://thiepmoi-hoplong.vercel.app/SVG/preview.jpg" />
+        <meta property="og:image" content="https://www.facebook.com/photo/?fbid=1134088408374700&set=pcb.1134090915041116" />
+        <meta property="og:image:secure_url" content="https://www.facebook.com/photo/?fbid=1134088408374700&set=pcb.1134090915041116" />
         <meta property="og:url" content="https://thiepmoi-hoplong.vercel.app/" />
         <meta property="og:image:width" content="1200" />
         <meta property="og:image:height" content="630" />
@@ -69,7 +69,7 @@ export default function RootLayout({
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         
         {/* Thêm link rel="image_src" */}
-        <link rel="image_src" href="https://thiepmoi-hoplong.vercel.app/SVG/preview.jpg" />
+        <link rel="image_src" href="https://www.facebook.com/photo/?fbid=1134088408374700&set=pcb.1134090915041116" />
         
         {/* Thêm thẻ og:locale */}
         <meta property="og:locale" content="vi_VN" />
